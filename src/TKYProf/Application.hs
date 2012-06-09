19b76f3b277cac9467ef91336be3f5d122c23aca
Mitsutoshi Aoe <maoe@foldr.in>
2012-06-09 23:45:35 +0900

    Run migration at bootup

--- src/TKYProf/Application.hs
+++ src/TKYProf/Application.hs
@@ -7,21 +7,23 @@ module TKYProf.Application
   ( makeApplication
   ) where
 
+import Database.Persist.GenericSql (runSqlPool, runMigration)
 import Database.Persist.Sqlite (withSqlitePool)
 import Network.Wai (Application)
 import Yesod.Core (toWaiApp)
 import Yesod.Default.Config (AppConfig, DefaultEnv)
-import Yesod.Default.Handlers (getRobotsR)
 import Yesod.Logger (Logger)
 import Yesod.Static (static)
 
-import TKYProf.Controller (TKYProf(..))
+import TKYProf.Controller
+import TKYProf.Model (migrateAll)
 import qualified Settings as S
 
 makeApplication :: AppConfig DefaultEnv () -> Logger -> IO Application
 makeApplication config logger = do
   st <- static S.staticDir
-  withSqlitePool ":memory:" 1 $ \pool ->
+  withSqlitePool ":memory:" 1 $ \pool -> do
+    runSqlPool (runMigration migrateAll) pool
     toWaiApp TKYProf { tkyConfig   = config
                      , tkyLogger   = logger
                      , tkyStatic   = st
