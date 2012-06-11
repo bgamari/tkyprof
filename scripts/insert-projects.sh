b25e6d330b2bbe6eb0260f1f023d7af9f719b8f0
Mitsutoshi Aoe <maoe@foldr.in>
2012-06-11 19:43:59 +0900

    Delete accidental text in insert-projects.sh

--- scripts/insert-projects.sh
+++ scripts/insert-projects.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in `seq 0 999`; do                                         /home/maoe/src/tkyprof
+for i in `seq 0 999`; do
   curl -X POST -d "{\"name\": \"Project #$i\"}" -D - http://localhost:3000/projects
 done
 
