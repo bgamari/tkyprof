6e72533057ee1f2561a019e597d192c30ae162d3
Mitsutoshi Aoe <maoe@foldr.in>
2012-06-28 21:59:48 +0900

    Add TimeAllocReport.create

--- src/TKYProf/Model/TimeAllocReport.hs
+++ src/TKYProf/Model/TimeAllocReport.hs
@@ -1,2 +1,27 @@
-module TKYProf.Model.TimeAllocReport where
+module TKYProf.Model.TimeAllocReport
+  ( module Database.Persist
+  , create
+  ) where
+import Control.Monad.Trans (MonadIO(..))
+import Data.Time.Clock (getCurrentTime)
+
+import Database.Persist
+
 import TKYProf.Model.Internal
+
+create
+  :: (MonadIO (b m), PersistStore b m)
+  => Key b (ProjectGeneric b)
+  -> Text
+  -> ByteString
+  -> b m (Entity (TimeAllocReportGeneric b))
+create projectId commandLine rawData = do
+  createdAt <- liftIO getCurrentTime
+  let report = TimeAllocReport
+        { timeAllocReportProjectId   = projectId
+        , timeAllocReportCommandLine = commandLine
+        , timeAllocReportRawData     = rawData
+        , timeAllocReportCreatedAt   = createdAt
+        }
+  rid <- insert report
+  return $ Entity rid report
