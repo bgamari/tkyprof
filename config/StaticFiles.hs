1c80c229386415f05d782a70104097abe04d6a09
Moritz Angermann <moritz.angermann@gmail.com>
2016-10-01 22:09:18 +0800

    Fix static files; needs a trailing slash now

--- config/StaticFiles.hs
+++ config/StaticFiles.hs
@@ -7,4 +7,4 @@ import Yesod.Static
 --   The upside to this is that you have compile-time verification that referenced files
 --   exist. However, any files added to your static directory during run-time can't be
 --   accessed this way. You'll have to use their FilePath or URL to access them.
-staticFiles "static"
+staticFiles "static/"
